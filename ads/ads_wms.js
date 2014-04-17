8a8278b8b6a2e52427b6400ea362767bb43dd3c7
Matt Walker <walkermatt@longwayaround.org.uk>
2014-04-17 19:58:34 +0100

    Inital commit of ADS OS Open WMS in British National Grid

---
+++ ads/ads_wms.js
@@ -0,0 +1,39 @@
+// Extent of the map in units of the projection
+var extent = [-3276800, -3276800, 3276800, 3276800];
+
+// Fixed resolutions to display the map at
+var resolutions = [1600, 800, 400, 200, 100, 50, 25, 10, 5, 2.5, 1, 0.5];
+
+// Basic ol3 Projection definition, include the extent here and specify the
+// resolutions as a property of the View2D or TileWMS if you are using a tiled
+// WMS to ensure tiles are requested at the correct boundaries
+var projection = new ol.proj.Projection({
+    code: 'EPSG:27700',
+    units: 'm',
+    extent: extent
+});
+
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.TileWMS({
+                url: 'http://t0.ads.astuntechnology.com/astuntechnology/osopen/service?',
+                attributions: [
+                    new ol.Attribution({html: 'OS OpenData, &copy; Ordnance Survey'})
+                ],
+                params: {
+                    'LAYERS': 'osopen',
+                    'FORMAT': 'image/png',
+                    'TILED': true
+                }
+            })
+        })
+    ],
+    view: new ol.View2D({
+        projection: projection,
+        resolutions: resolutions,
+        center: [315000, 468000],
+        zoom: 0
+    })
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+# OpenLayers 3 Examples using ADS and iShare services
+
+A collection of examples showing how [Astun Technology](http://astuntechnology.com/) services can be used with the [OpenLayers 3](http://ol3js.org/) client-side mapping library. To view simply clone the repo or download the zip and open each of the html files in a web browser.
+
+You can also view the example thanks to rawgithub.com here:
+
+* [ADS OS Open WMS](http://rawgithub.com/AstunTechnology/astun-ol3-examples/master/ads/ads_wms.html)
+
+Also see [astun-leaflet-examples](https://github.com/AstunTechnology/astun-leaflet-examples).
