cca64e14dea81dc8b838eb5bb2e9c133cb24b293
Matt Walker <walkermatt@longwayaround.org.uk>
2014-08-09 16:02:38 +0100

    Use Proj4js for the projection

--- ads/ads_wms.js
+++ ads/ads_wms.js
@@ -4,14 +4,14 @@ var extent = [-3276800, -3276800, 3276800, 3276800];
 // Fixed resolutions to display the map at
 var resolutions = [1600,800,400,200,100,50,25,10,5,2.5,1,0.5,0.25,0.125,0.0625];
 
-// Basic ol3 Projection definition used by the View. Include the extent here
-// and specify the resolutions as a property of the View to specify the zoom
-// levels that are available to the user.
-var projection = new ol.proj.Projection({
-    code: 'EPSG:27700',
-    units: 'm',
-    extent: extent
-});
+// Define British National Grid Proj4js projection (copied from http://epsg.io/27700.js)
+proj4.defs("EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs");
+
+// Define a projection based on the included Proj4js projection definition.
+// Include the extent here and specify the resolutions as a property of the
+// View to specify the zoom levels that are available to the user.
+var bng = ol.proj.get('EPSG:27700');
+bng.setExtent(extent);
 
 var map = new ol.Map({
     target: 'map',
@@ -37,7 +37,7 @@ var map = new ol.Map({
         })
     ],
     view: new ol.View({
-        projection: projection,
+        projection: bng,
         resolutions: resolutions,
         center: [413674, 289141],
         zoom: 0
